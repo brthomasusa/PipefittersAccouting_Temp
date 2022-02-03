e8d939649e7b15770c0f9d9ff7973eb3f157c19e
brthomasusa <brian.thomas@btechnical-consulting.com>
2022-02-03 17:22:47 -0600

    Testing common value objects

--- test/PipefittersAccounting.UnitTests/ValueObjects/Common/ValueObjectTests.cs
+++ test/PipefittersAccounting.UnitTests/ValueObjects/Common/ValueObjectTests.cs
@@ -1,3 +1,5 @@
+#pragma warning disable CS8600
+#pragma warning disable CS8604
 #pragma warning disable CS8625
 
 using System;
@@ -125,5 +127,122 @@ namespace PipefittersAccounting.UnitTests.ValueObjects.Common
 
             Assert.Equal("An organization name is required.", caughtException.ParamName);
         }
+
+        [Fact]
+        public void ShouldReturn_Valid_TelephoneNumber()
+        {
+            string phone = "972-555-5555";
+
+            var result = PhoneNumber.Create(phone);
+
+            Assert.IsType<PhoneNumber>(result);
+            Assert.Equal(phone, result);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_TelephoneNumber()
+        {
+            string phone = "0972-555-5555";
+
+            Action action = () => PhoneNumber.Create(phone);
+
+            var caughtException = Assert.Throws<ArgumentException>(action);
+
+            Assert.Contains("Invalid PhoneNumber number!", caughtException.Message);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_TelephoneNumberIsNull()
+        {
+            Action action = () => PhoneNumber.Create(null);
+
+            var caughtException = Assert.Throws<ArgumentNullException>(action);
+
+            Assert.Contains("The PhoneNumber number is required.", caughtException.Message);
+        }
+
+        [Fact]
+        public void ShouldReturn_Valid_Address_WithoutSecondAddressLine()
+        {
+            string line1 = "123 Main Street";
+            string line2 = null;
+            string city = "Anywhereville";
+            string stateCode = "TX";
+            string zipcode = "75216";
+
+            var result = Address.Create(line1, line2, city, stateCode, zipcode);
+
+            Assert.IsType<Address>(result);
+            Assert.Equal(line1, result.AddressLine1);
+            Assert.Null(result.AddressLine2);
+            Assert.Equal(city, result.City);
+            Assert.Equal(stateCode, result.StateCode);
+            Assert.Equal(zipcode, result.Zipcode);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_StateCodeDoesNotExist()
+        {
+            string line1 = "123 Main Street";
+            string line2 = null;
+            string city = "Anywhereville";
+            string stateCode = "IX";
+            string zipcode = "75216";
+
+            Action action = () => Address.Create(line1, line2, city, stateCode, zipcode);
+
+            var caughtException = Assert.Throws<ArgumentException>(action);
+
+            Assert.Contains("Invalid state code!", caughtException.Message);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_StateCodeIsNull()
+        {
+            string line1 = "123 Main Street";
+            string line2 = null;
+            string city = "Anywhereville";
+            string stateCode = null;
+            string zipcode = "75216";
+
+            Action action = () => Address.Create(line1, line2, city, stateCode, zipcode);
+
+            var caughtException = Assert.Throws<ArgumentNullException>(action);
+
+            Assert.Contains("A 2-digit state code is required.", caughtException.Message);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_ZipcodeIsNull()
+        {
+            string line1 = "123 Main Street";
+            string line2 = null;
+            string city = "Anywhereville";
+            string stateCode = "TX";
+            string zipcode = null;
+
+            Action action = () => Address.Create(line1, line2, city, stateCode, zipcode);
+
+            var caughtException = Assert.Throws<ArgumentNullException>(action);
+
+            Assert.Contains("A zip code is required.", caughtException.Message);
+        }
+
+        [Fact]
+        public void ShouldRaiseError_Invalid_Zipcode()
+        {
+            string line1 = "123 Main Street";
+            string line2 = null;
+            string city = "Anywhereville";
+            string stateCode = "TX";
+            string zipcode = "752136";
+
+            Action action = () => Address.Create(line1, line2, city, stateCode, zipcode);
+
+            var caughtException = Assert.Throws<ArgumentException>(action);
+
+            Assert.Contains("Invalid zip code!", caughtException.Message);
+        }
+
     }
 }
