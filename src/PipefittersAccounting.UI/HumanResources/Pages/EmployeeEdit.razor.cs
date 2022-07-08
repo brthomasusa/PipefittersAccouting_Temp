702db42bebe3ad2e3dfd31176b1fd7d8aea92937
Brian Thomas <brian.thomas@btechnical-consulting.com>
2022-07-08 07:59:44 -0500

    Code clean up

---
+++ src/PipefittersAccounting.UI/HumanResources/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using PipefittersAccounting.SharedModel.ReadModels;
+using PipefittersAccounting.SharedModel.Readmodels.HumanResources;
+using PipefittersAccounting.UI.Utilities;
+
+namespace PipefittersAccounting.UI.HumanResources.Pages
+{
+    public partial class EmployeeEdit
+    {
+
+    }
+}
